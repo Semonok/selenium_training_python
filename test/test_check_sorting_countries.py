81c507813290fd7430abeaa0d5b33033717d111a
Semonok <semonok95@gmail.com>
2018-05-02 00:40:22 +0500

    upgrade test

--- test/test_check_sorting_countries.py
+++ test/test_check_sorting_countries.py
@@ -1,11 +1,20 @@
 
-
 def test_sorting_countries(driver):
     i = 0
-    a=[]
+    a = []
     driver.find_element_by_link_text("Countries").click()
-    raws = driver.find_elements_by_xpath("//tbody/tr/td[5]/a")
+    raws = driver.find_elements_by_xpath("//tbody/tr")
     while i < len(raws):
         i += 1
-        a.append(driver.find_element_by_xpath("//tbody/tr[" + str(i) + "]/td[5]/a").text)
+        link = driver.find_element_by_xpath("//tbody/tr[" + str(i) + "]/td[5]/a")
+        a.append(link.text)
+        if driver.find_element_by_xpath("//tbody/tr[" + str(i) + "]/td [@class='text-center']").text != str(0):
+            link.click()
+            o = 0
+            b = []
+            while o < len(driver.find_elements_by_xpath("//tbody//tr//td[3]/input")):
+                o+=1
+                b.append(driver.find_element_by_xpath("//tbody//tr[" + str(o) + "]//td[3]/input").get_attribute("value"))
+            assert b == sorted(b)
+            driver.find_element_by_link_text("Countries").click()
     assert a == sorted(a)
