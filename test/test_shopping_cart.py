11ebbfe2fd4889f2e470a7dfdc3d1ccd26ea6453
Semonok <semonok95@gmail.com>
2018-05-10 00:49:37 +0500

    created test shopping cart

---
+++ test/test_shopping_cart.py
@@ -0,0 +1,35 @@
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+import random
+from selenium.webdriver.common.by import By
+
+def r_d(cash):
+    try:
+        return int(cash[1:])
+    except ValueError:
+        return int(float(cash[1:]))
+
+def test_shopping_cart(driver):
+    driver.find_element_by_css_selector("i.fa.fa-chevron-circle-left").click()
+    driver.find_element_by_css_selector("ul.nav.nav-stacked.nav-pills a").click()
+    for i in range(3):
+        duck = random.choice(driver.find_elements_by_css_selector("a.link[data-toggle='lightbox']"))
+        duck.click()
+        driver.find_element_by_css_selector("div#view-full-page a").click()
+        duck_size = driver.find_elements_by_css_selector("select.form-control")
+        if len(duck_size) > 0:
+            Select(duck_size[0]).select_by_value("Small")
+        driver.find_element_by_css_selector("button.btn.btn-success").click()
+        driver.wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "span.quantity"), str(i+1)))
+    driver.find_element_by_css_selector("div#cart a").click()
+    items = driver.find_elements_by_css_selector("button.btn.btn-danger")
+    for i in range(len(items)):
+        price = driver.find_element_by_xpath("//tr[@class='item']/td[5]").text
+        subtotal_price_path = "//table[@class='table table-striped table-bordered data-table']/tbody/tr[1]/td[2]"
+        subtotal_price = driver.find_element_by_xpath(subtotal_price_path).text
+        driver.find_element_by_css_selector("button.btn.btn-danger").click()
+        if i == len(items)-1:
+            assert driver.find_element_by_css_selector("div.cart.wrapper em").text == "There are no items in your cart."
+        else:
+            driver.wait.until(EC.text_to_be_present_in_element(
+            (By.XPATH, subtotal_price_path), "$" + str(r_d(subtotal_price) - r_d(price))))
