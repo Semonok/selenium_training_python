ef7b41ff818a7b22f78411013853b2d9a7e305b8
Semonok <semonok95@gmail.com>
2018-05-13 16:03:40 +0500

    created test for checking that links open in new windows

---
+++ test/test_check_link_in_edit_countries.py
@@ -0,0 +1,19 @@
+import random
+
+
+def test_check_links(driver):
+    driver.find_element_by_link_text("Countries").click()
+    i = random.choice(driver.find_elements_by_xpath("//tbody/tr"))
+    country_name = i.find_element_by_xpath("//td[5]").text
+    i.find_element_by_xpath("//td[7]/a").click()
+    assert country_name == driver.find_element_by_name("name").get_attribute("value")
+    for link in driver.find_elements_by_css_selector("form[name=country_form] a[target=_blank]"):
+        main_page = driver.current_window_handle
+        link.click()
+        driver.switch_to_window(driver.window_handles[1])
+        status = driver.execute_script("return document.readyState")
+        driver.wait.until(lambda s: status == "complete")
+        link_page = driver.current_window_handle
+        assert main_page != link_page
+        driver.close()
+        driver.switch_to_window(driver.window_handles[0])
--- training.py
+++ training.py
@@ -1,3 +1,7 @@
 t = '$3.55'
 p = float(t[1:])
 print(p)
+
+import string
+import random
+print(random.choice(string.digits))
